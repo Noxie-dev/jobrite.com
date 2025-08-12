d8527c002b9d414e8f2f839e0f10e3e54a9b73bc
Resup-dev <k.residd@gmail.com>
2025-08-13 01:24:46 +0200

    Initial Commit

---
+++ jobrite/jobriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  jobriteApp.swift
+//  jobrite
+//
+//  Created by Krwele Family on 2025/08/13.
+//
+
+import SwiftUI
+
+@main
+struct jobriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jobriteTests/jobriteTests.swift
@@ -0,0 +1,16 @@
+//
+//  jobriteTests.swift
+//  jobriteTests
+//
+//  Created by Krwele Family on 2025/08/13.
+//
+
+import Testing
+
+struct jobriteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
